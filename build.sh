5c84c7e32f16d307fbc7bdd3193856b4bfb5a6be
Steve Bennett <stevage@gmail.com>
2018-05-15 15:07:31 +1000

    README

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
+mkdir -p build
 node node_modules/browserify/bin/cmd.js -o build/index.js index.js
 
-# cp -pr lib build
+cp -pr lib build
 
 cp *.css build
-cp index-cdn.html build/index.html
+# cp index-cdn.html build/index.html
+
+cp index.html build/
